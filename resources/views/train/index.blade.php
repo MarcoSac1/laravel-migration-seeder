abb7087c2ef47009d6fc142524a374a23bd53214
MarcoSac1 <marcosaccone8@gmail.com>
2024-07-18 18:01:48 +0200

    created foreach

--- resources/views/train/index.blade.php
+++ resources/views/train/index.blade.php
@@ -1,5 +1,23 @@
 @extends('app')
 @section('main-section')
 <h1>Train schedule</h1>
-@dump($trains)
+
+    <ul>
+@foreach ($trains as $train)
+            <li>
+                <h1>
+                    {{ $train->azienda}}
+                </h1>
+                <p>
+                    Stazione di partenza: {{ $train ->stazione_di_partenza}}
+                    <br>
+                    Partenza giorno: {{ $train ->data_di_partenza}}
+                    <br>
+                    Orario di arrivo: {{ $train ->orario_di_arrivo}}
+                    <br>
+                    Codice Treno: {{ $train ->codice_treno}}
+                </p>
+            </li>
+@endforeach
+    </ul>
 @endsection
