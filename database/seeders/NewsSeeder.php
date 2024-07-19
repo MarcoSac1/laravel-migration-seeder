5da0c7ab1c5df7bd9b0fe87a13af57d998c55bf9
MarcoSac1 <marcosaccone8@gmail.com>
2024-07-19 16:58:40 +0200

    lock on code issue

---
+++ database/seeders/NewsSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class NewsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i=0; $i < 100; $i++) {
+            $news = new News();
+            $news ->title = $faker->unique()->sentence(5);
+            $news ->content = $faker->realText(500);
+            $news ->image_url = $faker->imageUrl(500,300,'news',true,'news: ');
+            $news ->date = $faker->dateTimeThisMonth();
+            $news ->save();
+        }
+    }
+}
