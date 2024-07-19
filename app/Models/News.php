0a7de88b7401f66f00309efa97405a9385a1b56a
MarcoSac1 <marcosaccone8@gmail.com>
2024-07-19 17:37:33 +0200

    fixed migration and timestaps issue

--- app/Models/News.php
+++ app/Models/News.php
@@ -5,8 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class News extends Model
 {
     use HasFactory;
     protected $table='news';
+    // per rimuovere l'errore Column not found: 1054 Unknown column 'updated_at' in 'field list'
+    public $timestamps=false;
 }
--- database/seeders/NewsSeeder.php
+++ database/seeders/NewsSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
-
+use App\Models\News;
 class NewsSeeder extends Seeder
 {
     /**
